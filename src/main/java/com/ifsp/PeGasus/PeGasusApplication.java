5ef5e97eb96f4c90f945f2f975c40af07348552a
3036171 <3036171@itp.local@LABFMAQ19.itp.local>
2026-05-18 07:52:07 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/ifsp/PeGasus/PeGasusApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.PeGasus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeGasusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeGasusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/PeGasus/PeGasusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.PeGasus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeGasusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
