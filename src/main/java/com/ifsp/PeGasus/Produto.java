af19c0e792a38e7d891529f1dac62da917f308c1
Paulo2306 <paulosbf2309@gmail.com>
2026-05-18 08:28:37 -0300

    feat: adicionei as funcionalidades CRUD para a classe produto

---
+++ src/main/java/com/ifsp/PeGasus/Produto.java
@@ -0,0 +1,61 @@
+package com.ifsp.PeGasus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "preco")
+    private long preco;
+
+
+    public Produto(String nome, String descricao, long preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    public Produto() {
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public long getPreco() {
+        return preco;
+    }
+    public void setPreco(long preco) {
+        this.preco = preco;
+    }
+    
+
+
+}
---
+++ src/main/java/com/ifsp/PeGasus/ProdutoController.java
@@ -0,0 +1,72 @@
+package com.ifsp.PeGasus;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ProdutoController {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping("formularioProduto")
+    public String formularioProduto(){
+        return "formularioProduto";
+    }
+
+    @PostMapping("/produtos")
+    public String saveProdutos(@RequestParam String nome, @RequestParam String descricao, @RequestParam long preco){
+        produtoRepository.save(new Produto(nome, descricao, preco));
+        return "redirect:/formularioProduto";
+    }
+
+    @GetMapping("/listaProdutos")
+    public String listProdutos(Model model){
+        List<Produto> listaProdutos = produtoRepository.findAll();
+        model.addAttribute("listaProdutos",listaProdutos);
+        return "listaProdutos";
+    }
+
+    @GetMapping("/produto/{id}")
+    public String produto(Model model, @PathVariable long id){
+        Produto produto = produtoRepository.findByID(id);
+        model.addAttribute("produto",produto);
+        return "detalhesProduto";
+    }
+
+
+    @GetMapping("/produto/{id}/editar")
+    public String editarProduto(@PathVariable long id, Model model){
+        Produto produto = produtoRepository.findByID(id);
+        model.addAttribute("produto", produto);
+        return "formularioEditarProduto";
+    }
+
+    @PostMapping("/produto/atualizarProduto")
+    public String atualizarProduto(@RequestParam long id,
+                                   @RequestParam String descricao,
+                                   @RequestParam long preco,
+                                    @RequestParam String nome){
+
+        Produto produto = produtoRepository.findByID(id);
+        produto.setDescricao(descricao);
+        produto.setPreco(preco);
+        produto.setNome(nome);
+        produtoRepository.update(produto);
+        return "redirect:/listaProdutos";
+    }
+
+    @GetMapping("/produto/{id}/excluirProduto")
+    public String excluirProduto(@PathVariable long id){
+        produtoRepository.delete(id);
+        return "redirect:/listaProdutos";
+    }
+
+}
