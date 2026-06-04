66b093aa9eb83241c754b9e294574f6de9889838
Guivipand <guivipand@gmail.com>
2026-06-04 10:08:57 -0300

    fix(2): Conflitos resolvidos

--- src/main/java/com/ifsp/PeGasus/Controller/CategoriaController.java
+++ src/main/java/com/ifsp/PeGasus/Controller/CategoriaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import com.ifsp.PeGasus.Model.Categoria;
 import com.ifsp.PeGasus.Repository.CategoriaRepository;
 
@@ -19,35 +20,36 @@ public class CategoriaController {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping("/categoria/formulario")
-    public String formulariocategoria(Model model) {
+    public String formulariocategoria(Model model){
         List<Categoria> listaCategorias = categoriaRepository.findAll();
         model.addAttribute("listaCategorias", listaCategorias);
         return "categoria/formularioCategoria";
     }
 
     @PostMapping("/categoria/cadastro")
-    public String savecategorias(@RequestParam String nome) {
+    public String savecategorias(@RequestParam String nome){
 
         categoriaRepository.save(new Categoria(nome));
-        return "redirect:/produto/lista";
+        return "redirect:/categoria/formulario";
     }
 
     @GetMapping("/categoria/lista")
-    public String listcategorias(Model model) {
+    public String listcategorias(Model model){
         List<Categoria> listacategorias = categoriaRepository.findAll();
-        model.addAttribute("listacategorias", listacategorias);
-        return "categoria/listaCategorias";
+        model.addAttribute("listaCategoria",listacategorias);
+        return "categoria/listaCategoria";
     }
 
+
     @GetMapping("/categoria/{id}/editar")
-    public String editarcategoria(@PathVariable long id, Model model) {
+    public String editarcategoria(@PathVariable long id, Model model){
         Categoria categoria = categoriaRepository.findById(id).orElse(null);
         model.addAttribute("categoria", categoria);
         return "categoria/formularioEditarCategoria";
     }
 
     @PostMapping("/categoria/atualizar")
-    public String atualizarcategoria(@RequestParam long id, @RequestParam String nome) {
+    public String atualizarcategoria(@RequestParam long id, @RequestParam String nome){
 
         Categoria categoria = categoriaRepository.findById(id).orElse(null);
         categoria.setNome(nome);
@@ -55,9 +57,9 @@ public class CategoriaController {
         return "redirect:/categoria/lista";
     }
 
-    @GetMapping("/categoria/{id}/excluircategoria")
-    public String excluircategoria(@PathVariable long id) {
+    @GetMapping("/categoria/{id}/excluirCategoria")
+    public String excluircategoria(@PathVariable long id){
         categoriaRepository.deleteById(id);
         return "redirect:/categoria/lista";
-    }
+    }   
 }
